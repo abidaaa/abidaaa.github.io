a87cfa4267875174d33be0d6865f2bcc45943787
Abida <abdesslame.abouibrahim@gmail.com>
2024-08-19 02:38:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    const camera = document.getElementById("model");
+    console.log(camera.style.height);
+
+})
